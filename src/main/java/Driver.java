1b6b7412a0fa620d93a15da2c0c5a5dcca768384
daniel-earley <danielse08@gmail.com>
2021-02-08 19:49:59 -0500

    added ability to enter own name

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,11 +1,24 @@
-public class main {
+import java.util.Scanner;
+public class Driver {
     public static void main(String[] args){
+        // Get Player Name
+        Scanner sc = new Scanner(System.in);
+        String name = "";
+
+        System.out.print("Please enter your name: ");
+        if (sc.hasNextLine()){
+            name = sc.nextLine();
+
+        }
+        
+
         // Initialize everything
         BattleSimulation bs = new BattleSimulation();
-        Player player = new Player("Daniel");
+        Player player = new Player(name);
         Orc orc1;
         Orc orc2;
         Gamble gamble;
+        
         // Main loop
         do {
             // Initialize Orcs and Gambling
@@ -14,7 +27,7 @@ public class main {
             gamble = new Gamble(orc1, orc2);
 
             // Print Orc Stats
-            System.out.println("~~~~~~~~~~Presenting Your Fighters!~~~~~~~~~~");
+            System.out.println("\n~~~~~~~~~~Presenting Your Fighters!~~~~~~~~~~");
             System.out.println(orc1);
             System.out.println(orc2);
             System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
@@ -35,6 +48,6 @@ public class main {
 
         } while (!player.isEmpty());
         System.out.printf("Sorry %s, but without any money you can't stay here!\n", player.getName());
-
+        sc.close();
     }
 }
